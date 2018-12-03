e6ef90a5799eaeab6df24502b39178c737b5677c
Charles Fourdrignier <charles@iteracode.fr>
2018-12-03 13:20:41 +0100

    Day3 - Part 2

--- src/day3.clj
+++ src/day3.clj
@@ -2,7 +2,7 @@
 
 (defn init-suit [n]
   (vec (for [x (range n)]
-         (vec (repeat n ".")))))
+         (vec (repeat n #{})))))
 
 #_(init-suit 5)
 
@@ -17,14 +17,9 @@
 
 #_(parse-line "#123 @ 3,2: 5x4")
 
-(defn update-inch [value old-value]
-  (if (= old-value ".")
-    value
-    "X"))
+(def update-inch conj)
 
-#_(update-inch "123" ".")
-#_(update-inch "123" "2")
-#_(update-inch "123" "X")
+#_(update-inch #{} "value")
 
 (defn claim->coords [{left :left top :top width :width height :height}]
   (for [x (range left (+ width left))
@@ -37,18 +32,20 @@
   (let [value (:id claim)
         coords (claim->coords claim)]
     (reduce
-     (fn [acc coord] (update-in acc coord (partial update-inch value)))
+     (fn [acc coord] (update-in acc coord #(update-inch % value)))
      suit
      coords)))
 
 #_(update-suit (init-suit 11) { :id "#" :left 3 :top 2 :width 5 :height 4})
 
+(defn more-than-one [s] (> (count s) 1))
+
 (defn count-overlap [suit]
   (->> suit
-       (mapcat #(filter #{"X"} %))
+       (mapcat #(filter more-than-one  %))
        count))
 
-#_(count-overlap [[ "X" "X"] ["X" "."]])
+#_(count-overlap [[#{1, 2} #{2 3}] [#{1 2} #{1}]])
 
 (def example "#1 @ 1,3: 4x4
   #2 @ 3,1: 4x4
@@ -56,12 +53,29 @@
 
 (def data (slurp "src/day3.input"))
 
-(->> data
-     (clojure.string/split-lines)
-     (map clojure.string/trim)
-     (map parse-line)
+(def claims (->> data
+                 (clojure.string/split-lines)
+                 (map clojure.string/trim)
+                 (map parse-line)
+                 ))
+;; part-1
+(->> claims
      (reduce
       update-suit
       (init-suit 1000))
      (count-overlap)
+     )
+
+(->> claims
+     (reduce
+      update-suit
+      (init-suit 1000))
+     (mapcat identity)
+     (reduce (fn [acc inch]
+               (if (more-than-one inch)
+                 (update acc :overlaps clojure.set/union inch)
+                 (update acc :free clojure.set/union inch)))
+             { :overlaps #{} :free #{}})
+     ((fn [{ overlaps :overlaps free :free }]
+        (clojure.set/difference free overlaps)))
      )
